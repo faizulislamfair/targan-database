20f6be86151b0a3e72d7b74d3d2e5410084cfa90
faizulislamfair <faizulislamfair@gmail.com>
2024-05-30 02:44:02 +0600

    feat: char2 to 6 contents added

--- pages/chart7.js
+++ pages/chart7.js
@@ -2,7 +2,45 @@ import React from 'react';
 
 const chart7 = () => {
     return (
-        <div>
+        <div className='p-5'>
+
+            <div className='text-[#1C47F2] font-bold text-[20px]'>
+                Chart 2. Experiment Summary
+            </div>
+
+            <div className="bg-[#F2DCDB] p-3 border-2 border-black mt-3">
+                <u><b>Experiment Details</b></u> <br />
+                <b>Clinical Manifestation:</b> <br />
+                IdiopathicASD and Neurotypical Control <br />
+                <b>Comparison Group:</b> <br />
+                Idiopathic ASD Individuals vs Neurotypical Controls <br />
+                <b>Organ: </b> <br />
+                Postmortem Cerebral Cortex <br />
+                <b>Comparing Organ Type: </b> <br />
+                Brodmann Area 9 (Frontal Lobe) <br />
+                <b>Number of Subjects/Samples: </b> <br />
+                i. Brodmann Area 9 (Idiopathic ASD): 41
+                ii. Brodmann Area 9 (Control): 45
+
+                <b>Sample Characteristics: </b> <br />
+                i. ASD Cortex: Brodmann Area 9 of Frontal Lobe
+                ii. Control Cortex: Brodmann Area 9 of Frontal Lobe
+
+                <b>Data Type: </b> <br />
+                RNA-seq <br />
+
+                <b>Gene Expression: </b> <br />
+                mRNA/Non-coding RNA <br />
+
+                <b>Species: </b> <br />
+                Human <br />
+                <b>Experiment Name:</b> <br />
+                Broad Transcriptomic Dysregulation Occurs Across the Cerebral Cortex in ASD <br />
+
+                <u><b>References</b></u> <br />
+                Gandal MJ, Haney JR, Wamsley B, et al. Broad transcriptomic dysregulation occurs across the cerebral cortex in ASD. Nature. 2022;611(7936):532-539. <br />
+
+            </div>
 
         </div>
     );
